debb6bf4799329858a37e1e9f2bcdebec0db72f1
Meghrajsinh Vaghela <vaghelameghrajsinh96@gmail.com>
2026-09-23 20:06:50 +0530

    intial commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+    <h1 id='child1'>Hello</h1>
+    <h2 id='child2'>hello</h2>
+    </div>
+
+    <div id='chacha'>
+        <h1 id='chacha1'>hello</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
+
